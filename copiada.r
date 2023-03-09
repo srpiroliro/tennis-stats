2f05ae24756feb1c66891a71f90b755d952fb70b
piroliro <56773568+srpiroliro@users.noreply.github.com>
2023-03-10 00:40:45 +0100

    Add files via upload

--- copiada.r
+++ copiada.r
@@ -2,12 +2,27 @@ library(dplyr)
 library(lubridate)
 library(tidyverse)
 
-df <- read.csv('w_stats/2000.csv')
-for (i in 2001:2022){
-  str_temp <- paste("w_stats/",i,'.csv',sep="")
+
+# MAN
+df <- read.csv('stats/1968.csv')
+for (i in 1969:2022){
+  str_temp <- paste("stats/",i,'.csv',sep="")
   df_temp <- read.csv(str_temp)
   df <- rbind(df,df_temp)
 }
+
+# WOMAN
+# df <- read.csv('w_stats/1968.csv')
+# for (i in 1969:2022){
+#   str_temp <- paste("stats/",i,'.csv',sep="")
+#   df_temp <- read.csv(str_temp)
+#   df <- rbind(df,df_temp)
+# }
+
+
+#####################################################################################################
+#####################################################################################################
+
 
 df <- df %>%
   mutate(tournament_date = as.Date(as.character(tourney_date),"%Y%m%d"),
@@ -15,7 +30,6 @@ df <- df %>%
 
 
 print("OK 1")
-
 
 df_subset <- df %>%
   select(tourney_id,tourney_name,surface,round,tourney_level,tourney_date,winner_id,winner_seed,winner_hand,winner_age,winner_ht,
@@ -26,10 +40,6 @@ df_subset <- df %>%
          rank_diff = winner_rank - loser_rank)
 
 print("OK 2")
-
-
-# tournament_date
-
 
 df_win_experience <- df %>% 
   #filter(tourney_level=='G') %>%
@@ -91,9 +101,7 @@ df_subset_winner <- df_subset %>%
   left_join(df_win_experience_last3 %>% select(winner_id,tourney_id,last_result)) %>%
   select(surface,round,tourney_level,age_diff,ht_diff,rank_diff,experience,last_result) %>%
   drop_na() %>%
-  mutate(win_lose = 1) 
-
-
+  mutate(win_lose = -1) 
 
 print("OK 7")
 
@@ -102,7 +110,7 @@ df_subset_loser <- df_subset %>%
   left_join(df_lost_experience_last3 %>% select(loser_id,tourney_id,last_result)) %>%
   select(surface,round,tourney_level,age_diff,ht_diff,rank_diff,experience,last_result) %>%
   drop_na() %>%
-  mutate(win_lose = -1) 
+  mutate(win_lose = 1) 
 
 print("OK 8")
 
@@ -112,47 +120,37 @@ df_model <- df_subset_winner  %>%
 
 print("OK 9")
 
+
+#####################################################################################################
+#####################################################################################################
+
+
 #### Statistical tests
 
-# library(ggplot2)
-# library(ggpubr)
-# library(tidyverse)
-# library(broom)
-# library(AICcmodavg)
 
-
-
-
+# SEPARETING DATA
 set.seed(as.numeric(Sys.time(), units = "secs"))
-
-train_indices <- sample(nrow(df_model), size = round(0.8 * nrow(df_model)), replace = FALSE)
+train_indices <- sample(nrow(df_model), size = round(0.9 * nrow(df_model)), replace = FALSE)
 train <- df_model[train_indices, ]
 test <- df_model[-train_indices, ]
 
-# lm = lm(win_lose~surface+age_diff+ht_diff+rank_diff+experience+last_result+tourney_level, data = train) #Create the linear regression
-# summary(lm)
+
+
+# ORIGINAL
+lm = lm(win_lose~rank_diff+experience+last_result+surface+age_diff+ht_diff+tourney_level, data = train)
+pp<-predict(lm, test)
+
+
+# OUR DATA
+# lm = lm(win_lose~rank_diff+experience+surface+ht_diff, data = train) 
 # pp<-predict(lm, test)
 
-# lmmm = lm(win_lose~surface+tourney_level+age_diff+ht_diff+rank_diff+experience+last_result, data=train) #Create the linear regression
-# pp<-predict(lmmm, test)
-
-glmmm = glm(win_lose~surface+tourney_level+age_diff+ht_diff+rank_diff+experience+last_result, data=train,family = binomial,control = list(maxit = 2, epsilon=1)) #Create the logistic regression
-summary(glmmm)
-pp<-predict(glmmm, test)
-
-# lmm2 <- lm(win_lose~surface+tourney_level+age_diff+ht_diff+rank_diff+experience, data=train) #Create the linear regression
-# pp<-predict(lmm2, test)
-
-# lmmm = lm(win_lose~surface+tourney_level+age_diff+ht_diff+rank_diff+experience+last_result, data=train) #Create the linear regression
-# pp<-predict(glmmm, test)
 
 
-
-actuals_preds <- data.frame(cbind(actuals=test$win_lose, predicteds=pp))  # make actuals_predicteds dataframe.
+# TABLE
+actuals_preds <- data.frame(cbind(actuals=test$win_lose, predicteds=pp))
 cor(actuals_preds)  # 82.7%
 
 
-min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
-# => 58.42%, min_max accuracy
-mape <- mean(abs((actuals_preds$predicteds - actuals_preds$actuals))/actuals_preds$actuals)  
-# => 48.38%, mean absolute percentage deviation
+# min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
+# mape <- mean(abs((actuals_preds$predicteds - actuals_preds$actuals))/actuals_preds$actuals)  
